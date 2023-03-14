0cd4e3a6897cfeb1937c6818b55308d13d900033
Ravi K R <raviram.mysore@gmail.com>
2023-03-14 09:25:39 +0530

    Add project files.

---
+++ ConnectedMode/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ConnectedDemo;
+class Program
+{
+    static string server = "(localdb)";
+    static string instance = "mssqllocaldb";
+    static string database = "StudentDB";
+    static string authentication = "Integrated Security = true";
+
+    static string ConString = $"Data Source={server}\\{instance}; Initial Catalog={database};{authentication}";
+    static void Main(string[] args)
+    {
+        WriteData();
+        ReadData();
+        Console.ReadKey();
+    }
+
+    static void ReadData()
+    {
+        try
+        {
+            using (SqlConnection connection = new SqlConnection(ConString))
+            {
+                // Creating SqlCommand objcet   
+                SqlCommand cm = new SqlCommand("select * from student", connection);
+
+                // Opening Connection  
+                connection.Open();
+
+                // Executing the SQL query  
+                SqlDataReader sdr = cm.ExecuteReader();
+                while (sdr.Read())
+                    Console.WriteLine(sdr["Name"] + ",  " + sdr["Email"] + ",  " + sdr["Mobile"]);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("OOPs, something went wrong.\n" + e);
+        }
+    }
+    static void WriteData()
+    {
+        try
+        {
+            using (SqlConnection connection = new SqlConnection(ConString))
+            {
+                SqlCommand cmd = new SqlCommand("insert into Student values (105, 'Ramesh', 'Ramesh@dotnettutorial.net', '1122334455')", connection);
+                connection.Open();
+                
+                int rowsAffected = cmd.ExecuteNonQuery();
+                Console.WriteLine("Inserted Rows = " + rowsAffected);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("OOPs, something went wrong.\n" + e);
+        }
+    }
+}
---
+++ DisconnectedMode/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DisconnectedDemo;
+class Program
+{
+    static string server = "(localdb)";
+    static string instance = "mssqllocaldb";
+    static string database = "StudentDB";
+    static string authentication = "Integrated Security = true";
+
+    static string ConString = $"Data Source={server}\\{instance}; Initial Catalog={database};{authentication}";
+    static void Main(string[] args)
+    {
+        WriteData();
+        ReadData();
+        Console.ReadKey();
+    }
+    static void ReadData()
+    {
+        try
+        {
+            using (SqlConnection connection = new SqlConnection(ConString))
+            {
+                //Create the SqlDataAdapter instance by specifying the command text and connection object
+                SqlDataAdapter dataAdapter = new SqlDataAdapter("select * from student", connection);
+                //Creating DataSet Object
+                DataSet dataSet = new DataSet();
+                //Filling the DataSet using the Fill Method of SqlDataAdapter object
+                //Here, we have not specified the data table name and the data table will be created at index position 0
+                dataAdapter.Fill(dataSet);
+                //Iterating through the DataSet 
+                //First fetch the Datatable from the dataset and then fetch the rows using the Rows property of Datatable
+                foreach (DataRow row in dataSet.Tables[0].Rows)
+                {
+                    //Accessing the Data using the string column name as key
+                    Console.WriteLine(row["Id"] + ",  " + row["Name"] + ",  " + row["Email"]);
+                    //Accessing the Data using the integer index position as key
+                    //Console.WriteLine(row["Id"] + ",  " + row["Name"] + ",  " + row["Mobile"]);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Exception Occurred: {ex.Message}");
+        }
+        Console.ReadKey();
+    }
+    static void WriteData()
+    {
+        try
+        {
+            using (SqlConnection connection = new SqlConnection(ConString))
+            {
+                //Create the SqlDataAdapter instance by specifying the command text and connection object
+                SqlDataAdapter dataAdapter = new SqlDataAdapter("SELECT * FROM Student", connection);
+
+                // At this point SqlCommandBuilder should generate T-SQL statements automatically
+                SqlCommandBuilder commandBuilder = new SqlCommandBuilder(dataAdapter);
+
+                //Creating DataSet Object
+                DataSet dataSet = new DataSet();
+                //Filling the DataSet using the Fill Method of SqlDataAdapter object
+                dataAdapter.Fill(dataSet);
+
+                //Now Update First Row i.e. Index Position 0
+                DataRow dataRow = dataSet.Tables[0].Rows[0];
+                dataRow["Name"] = "Name Updated";
+
+                //Provide the DataSet and the DataTable name to the Update method
+                //Here, SqlCommandBuilder will automatically generate the UPDATE SQL Statement 
+                int rowsUpdated = dataAdapter.Update(dataSet, dataSet.Tables[0].TableName);
+
+                if (rowsUpdated == 0)
+                {
+                    Console.WriteLine("\nNo Rows Updated");
+                }
+                else
+                {
+                    Console.WriteLine($"\n{rowsUpdated} Row(s) Updated");
+                }
+
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("OOPs, something went wrong.\n" + e);
+        }
+    }
+}
